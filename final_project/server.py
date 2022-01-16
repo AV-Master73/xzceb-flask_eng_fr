6af3a5f21fb688a1ccc79605f074d87a69b80485
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2022-01-16 23:06:45 +0200

    endpoints added

--- final_project/server.py
+++ final_project/server.py
@@ -7,14 +7,14 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    return translator.english_to_french(textToTranslate)
+    # return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    return translator.french_to_english(textToTranslate)
+    # return "Translated text to English"
 
 @app.route("/")
 def renderIndexPage():
