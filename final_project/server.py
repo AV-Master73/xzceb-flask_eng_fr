8d0348c4303136a9f084ae914f8085ebfbfa19b2
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2022-01-16 23:42:19 +0200

    Final release

--- final_project/server.py
+++ final_project/server.py
@@ -20,5 +20,6 @@ def frenchToEnglish():
 def renderIndexPage():
     # Write the code to render template
     return render_template('index.html')
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
